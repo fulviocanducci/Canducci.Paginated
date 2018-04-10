6973cb52d59dda00c6a8bd1811634df9f7c7a64c
fulviocanducci <fulviocanducci@hotmail.com>
2018-04-10 14:12:26 -0300

    HtmlHelpersPagination Versio 1.0-Beta

--- Canducci.Pagination.Mvc/HtmlHelpersExtensions.cs
+++ Canducci.Pagination.Mvc/HtmlHelpersExtensions.cs
@@ -4,14 +4,16 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Linq.Expressions;
 //https://github.com/netdragoon/helpWebForms/blob/master/Canducci.HtmlHelpers/MethodsHelpers.cs
 namespace Canducci.Pagination.Mvc
 {
     public static class HtmlHelpers
     {
-        #region internal
+        #region getValue
         internal static object GetValue<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression)
             where TModel : class
         {
@@ -24,6 +26,8 @@ namespace Canducci.Pagination.Mvc
             return func(model);
         }
         #endregion
+
+        #region stringContent
         public static string ToStringContent(this IHtmlContent content)
         {
             using (var writer = new StringWriter())
@@ -32,36 +36,118 @@ namespace Canducci.Pagination.Mvc
                 return writer.ToString();
             }
         }
-        
-        public static IHtmlContent Page<T>(this IHtmlHelper htmlHelper, Paginated<T> paginated)            
-        {            
-            TagBuilder tagUl = new TagBuilder("ul");
-            tagUl.MergeAttribute("class", "pagination");
-            if (paginated.IsFirstPage)
+        #endregion
+
+        internal static IHtmlContentBuilder AppendHtmlTaLi(this IHtmlContentBuilder content, TagBuilder tag, string cssClass = null)
+        {
+            TagBuilder li = new TagBuilder("li")
             {
-                tagUl.InnerHtml.AppendHtmlLine($"<li class=\"page-item disabled\"><a class=\"page-link\" href=\"#\" tabindex=\"-1\">«</a></li>");
-                tagUl.InnerHtml.AppendHtmlLine($"<li class=\"page-item disabled\"><a class=\"page-link\" href=\"#\" tabindex=\"-1\">« voltar</a></li>");
-            }
-            else
-            {
-                tagUl.InnerHtml.AppendHtmlLine($"<li class=\"page-item\">{htmlHelper.ActionLink("«", "Index", new { Page = 1 }).ToStringContent()}</li>");
-                tagUl.InnerHtml.AppendHtmlLine($"<li class=\"page-item\">{htmlHelper.ActionLink("« voltar", "Index", new { Page = paginated.HasPreviousPage ? paginated.PageNumber - 1 : paginated.PageNumber }).ToStringContent()}</li>");
-            }
-            tagUl.InnerHtml.AppendHtml("<li class=\"page-item active\">");
-            tagUl.InnerHtml.AppendHtml($"<a class=\"page -link\" href=\"#\">{paginated.PageNumber} / {paginated.PageCount} <span class=\"sr-only\">(current)</span></a>");
-            tagUl.InnerHtml.AppendHtml("</li>");
-            if (!paginated.IsLastPage)
-            {
-                tagUl.InnerHtml.AppendHtmlLine($"<li class=\"page-item\">{htmlHelper.ActionLink("avançar »", "Index", new { Page = paginated.HasNextPage ? paginated.PageNumber + 1 : paginated.PageNumber }).ToStringContent()}</li>");
-                tagUl.InnerHtml.AppendHtmlLine($"<li class=\"page-item\">{htmlHelper.ActionLink("»", "Index", new { Page = paginated.PageCount }).ToStringContent()}</li>");
-            }
-            else
-            {
-                tagUl.InnerHtml.AppendHtmlLine($"<li class=\"page-item disabled\"><a class=\"page-link\" href=\"#\" tabindex=\"-1\">avançar »</a></li>");
-                tagUl.InnerHtml.AppendHtmlLine($"<li class=\"page-item disabled\"><a class=\"page-link\" href=\"#\" tabindex=\"-1\">»</a></li>");                
-            }
-            
-            return new HtmlString(tagUl.ToStringContent());
+                TagRenderMode = TagRenderMode.Normal
+            };
+            if (!string.IsNullOrEmpty(cssClass)) li.AddCssClass(cssClass);
+            li.InnerHtml.AppendHtml(tag);            
+            content.AppendHtml(li);
+            content.AppendHtmlLine(Environment.NewLine);
+            return content;
         }
+
+        internal static TagBuilder TagLink(string url, string label, string cssClass = null)
+        {
+            TagBuilder tag = new TagBuilder("a");
+            tag.Attributes.Add("href", url);
+            if (!string.IsNullOrEmpty(cssClass))
+            {
+                tag.Attributes.Add("class", cssClass);
+            }
+            tag.InnerHtml.Append(label);            
+            return tag;
+        }
+        internal static HtmlContentBuilder CreateHtmlContentBuilder(PaginatedOptions options)
+        {
+            HtmlContentBuilder content = new HtmlContentBuilder();
+            content.AppendHtml($"<ul class=\"{options.CssClassUl}\">");
+            content.AppendHtmlLine(Environment.NewLine);
+            return content;
+
+        }
+        public static IHtmlContent PaginationNumbersWithPreviousNext<T>(this IHtmlHelper htmlHelper, Paginated<T> paginated, Func<int, string> generatePageUrl, PaginatedOptions options = null)
+        {
+            if (options == null) options = new PaginatedOptions();
+            HtmlContentBuilder content = CreateHtmlContentBuilder(options);            
+            Previous(content, htmlHelper, paginated, generatePageUrl, options);
+            Numbers(content, htmlHelper, paginated, generatePageUrl, options);
+            Next(content, htmlHelper, paginated, generatePageUrl, options);
+            content.AppendHtml("</ul>");
+            return content;
+        }
+
+        #region previous_next
+        internal static HtmlContentBuilder Previous<T>(HtmlContentBuilder content, IHtmlHelper htmlHelper, Paginated<T> paginated, Func<int, string> generatePageUrl, PaginatedOptions options)
+        {
+            TagBuilder aPrevious = TagLink(paginated.IsFirstPage ? "#" : generatePageUrl(paginated.PageNumber - 1), 
+                options.PreviousLabel, 
+                options.CssClassLink);
+            content.AppendHtmlTaLi(aPrevious, paginated.IsFirstPage ? options.CssClassLiDisabled : null);
+            return content;            
+        }
+        internal static HtmlContentBuilder Next<T>(HtmlContentBuilder content, IHtmlHelper htmlHelper, Paginated<T> paginated, Func<int, string> generatePageUrl, PaginatedOptions options)
+        {
+            TagBuilder aNext = TagLink(paginated.IsLastPage ? "#": generatePageUrl(paginated.PageNumber + 1), 
+                options.NextLabel, 
+                options.CssClassLink);
+            content.AppendHtmlTaLi(aNext, paginated.IsLastPage ? options.CssClassLiDisabled : null);
+            return content;
+        }
+        public static IHtmlContent PaginationPreviousNext<T>(this IHtmlHelper htmlHelper, Paginated<T> paginated, Func<int, string> generatePageUrl, PaginatedOptions options = null)
+        {
+            if (options == null) options = new PaginatedOptions();
+            HtmlContentBuilder content = CreateHtmlContentBuilder(options);
+            Previous(content, htmlHelper, paginated, generatePageUrl, options);
+            Next(content, htmlHelper, paginated, generatePageUrl, options);
+            content.AppendHtml("</ul>");
+            return content;
+        }
+        #endregion
+
+        #region numbers
+        internal static HtmlContentBuilder Numbers<T>(HtmlContentBuilder content, IHtmlHelper htmlHelper, Paginated<T> paginated, Func<int, string> generatePageUrl, PaginatedOptions options)
+        {            
+            PaginatedMetaData metaData = paginated.ToPaginatedMetaData();
+            int interaction = 3;
+            List<int> pages = new List<int>();
+            int pageNumber = paginated.PageNumber;
+            while (interaction >= 0 && pageNumber > 0)
+            {
+                pages.Add(pageNumber--);
+                interaction--;
+            }
+            interaction = 0;
+            pageNumber = paginated.PageNumber;
+            while (interaction < 3 && pageNumber < metaData.PageCount)
+            {
+                pages.Add(++pageNumber);
+                interaction++;
+            }
+            pages.OrderBy(o => o).ToList()
+                .ForEach(x =>
+                {
+                    var tagLink = TagLink((metaData.PageNumber != x) ? generatePageUrl(x) : "#", x.ToString(), options.CssClassLink);
+                    content.AppendHtmlTaLi(tagLink, (metaData.PageNumber == x) 
+                        ? options.CssClassLiActive + " " + options.CssClassLi
+                        : options.CssClassLi);
+                });           
+            return content;
+        }
+
+        public static IHtmlContent PaginationNumbers<T>(this IHtmlHelper htmlHelper, Paginated<T> paginated, Func<int, string> generatePageUrl, PaginatedOptions options = null)
+        {
+            if (options == null) options = new PaginatedOptions();
+            HtmlContentBuilder content = CreateHtmlContentBuilder(options);
+            Numbers(content, htmlHelper, paginated, generatePageUrl, options);
+            content.AppendHtml("</ul>");
+            return content;
+        }
+        #endregion
+
     }
 }
---
+++ Canducci.Pagination.Mvc/PaginatedOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Canducci.Pagination.Mvc
+{
+    public class PaginatedOptions
+    {
+        public string CssClassLi { get; set; } = "page-item";
+        public string CssClassLiDisabled { get; set; } = "disabled";
+        public string CssClassUl { get; set; } = "pagination";
+        public string CssClassLiActive { get; set; } = "active";
+        public string CssClassLink { get; set; } = "page-link";
+
+        public string PreviousLabel { get; set; } = "Previous";
+        public string NextLabel { get; set; } = "Next";
+    }
+}
